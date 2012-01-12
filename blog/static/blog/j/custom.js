9a4ecc0b6db3ab02ddfa6251d3781b68182aee6b
Simon Winter <simon@simonwinter.co.nz>
2012-01-13 12:12:58 +1300

    Added rollovers on list pages. Added captions to images.

--- blog/static/blog/j/custom.js
+++ blog/static/blog/j/custom.js
@@ -1,3 +1,43 @@
 jQuery(document).ready(function($) {
 	$('.entry_list article').equalHeightColumns();
+	
+	/* ------ Captions --------- */
+	$('img[alt!=""]:not(.greyscale):not(.rollover)').each(function() {
+		var im = $(this).clone(),
+			container = $('<div class="image_caption">');
+		
+		container.append(im);
+		im.after(
+			$('<div class="show">').append('<span>+</span>')
+		);
+		
+		$(this).replaceWith(container);
+		$('.show').after($('<div class="caption_text">').append(
+			$('<span class="caption">').text(im.attr('alt'))
+		));
+		$('.caption').after(
+			$('<span class="close">').text('-')
+		);
+		
+		$('.show').click(function() {
+			//$(this).slideUp();
+			$('.caption_text').slideDown();
+		});
+		
+		$('.close').click(function() {
+			$('.caption_text').slideUp();
+			//$('.show').slideDown();
+		});
+	});
+	
+	/* ------ Entry list rollovers ----------- */
+	$('.entry_list article').hover(function() {
+		$(this).find('.rollover').hide().fadeIn(400);
+	}, function() {
+		$(this).find('.rollover').show().fadeOut(400);
+	});
+	
+	$('.entry_list article').click(function() {
+		window.location.href = $(this).find('a:first').attr('href');
+	});
 });
