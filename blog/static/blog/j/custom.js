45b64b994cb2532622545220903eb330cd7c557c
Simon Winter <simon@simonwinter.co.nz>
2012-01-13 13:08:34 +1300

    Added collapsable headers.

--- blog/static/blog/j/custom.js
+++ blog/static/blog/j/custom.js
@@ -20,13 +20,11 @@ jQuery(document).ready(function($) {
 		);
 		
 		$('.show').click(function() {
-			//$(this).slideUp();
 			$('.caption_text').slideDown();
 		});
 		
 		$('.close').click(function() {
 			$('.caption_text').slideUp();
-			//$('.show').slideDown();
 		});
 	});
 	
@@ -40,4 +38,44 @@ jQuery(document).ready(function($) {
 	$('.entry_list article').click(function() {
 		window.location.href = $(this).find('a:first').attr('href');
 	});
+	
+	/* ------ collapsable Banner. --------------- */
+	var header_height = $('header').height(),
+		logo_height = $('.title').outerHeight();
+	
+	$('header .grid_3.tags').after($('<div class="grid_3 omega">').html('<span>-</span>'));
+	
+	$('header .grid_3.omega').delegate('span', 'click', function() {
+		var h = logo_height,
+			t = '+',
+			that = this;
+		
+		if($(this).hasClass('clicked')) {
+			var h = header_height,
+				t = '-';
+		};
+		
+		$('header, header .grid_3:not(.omega)').animate({'height': h}, 500, function() {
+			$(that).text(t);
+		});
+		
+		$('header .grid_3 ul').slideToggle();
+		$(this).toggleClass('clicked');
+	});
+	
+	/*
+$('header .grid_3.omega span.clicked').click(function() {
+		var that = this;
+		
+		$('header, header .grid_3:not(.omega)').animate({'height': header_height}, 500, function() {
+			$(that).text('-');
+			$('header .grid_3 ul').slideUp();
+			return false;
+		});
+		
+		$(this).removeClass('clicked');
+	});
+*/
+	
+
 });
